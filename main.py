e21d04eeffb7c839bdf7fe85fbd6f7859f2e55b3
ishaansavla <ishaan.savla16@gmail.com>
2023-06-19 12:06:07 -0700

    Added Features

--- main.py
+++ main.py
@@ -11,10 +11,16 @@ def main():
     # overtime_wage = overtime * ov_rate
     # regular_wage = reg_hours * reg_rate
     # total_wage = regular_wage + overtime_wage
+    overtime = workhours - reg_hours
+    overtime_wage = ov_rate * overtime
+    regular_wage = reg_rate * reg_hours
+    total_wage = regular_wage + overtime_wage
 
-    print(f"Regular hours: {reg_hours} Regular Charge: {regular_wage}")
-    print(f"Overtime hours: {overtime} Overtime Charge: {overtime_wage:.2f}")
-    print(f"Total wage : {total_wage:.2f}")
+    print("")
+    print(f"Regular Charge: ${regular_wage}")
+    print(f"Overtime Charge: ${overtime_wage:.2f}")
+    print(f"Total wage : ${total_wage:.2f}")
+    print("")
 
    ##################################################
    # Do not delete the return statement
